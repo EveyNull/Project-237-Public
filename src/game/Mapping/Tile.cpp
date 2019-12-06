bb64b0c6f982325710129a260533a41e766f8e90
Evey Watson <evey2.watson@live.uwe.ac.uk>
2019-12-06 13:12:27 +0000

    Tile class

---
+++ src/game/Mapping/Tile.cpp
@@ -0,0 +1,5 @@
+//
+// Created by e22-watson on 06/12/2019.
+//
+
+#include "Tile.h"
---
+++ src/game/Mapping/Tile.h
@@ -0,0 +1,14 @@
+//
+// Created by e22-watson on 06/12/2019.
+//
+
+#ifndef THE_SHINING_GAME_PROJECT_237_TILE_H
+#define THE_SHINING_GAME_PROJECT_237_TILE_H
+
+class Tile
+{
+ private:
+  bool walkable = false;
+};
+
+#endif // THE_SHINING_GAME_PROJECT_237_TILE_H

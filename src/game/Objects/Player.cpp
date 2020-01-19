e656a8c68b281583915c9a30695b5bac7b935349
Kirsten <Kirsten2.wotherspoon@live.uwe.ac.uk>
2020-01-19 14:04:42 +0000

    It's empty but we need one

---
+++ src/game/Objects/Player.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Kirsten on 19/01/2020.
+//
+
+#include "Player.h"
---
+++ src/game/Objects/Player.h
@@ -0,0 +1,12 @@
+//
+// Created by Kirsten on 19/01/2020.
+//
+
+#ifndef THE_SHINING_GAME_PROJECT_237_PLAYER_H
+#define THE_SHINING_GAME_PROJECT_237_PLAYER_H
+
+class Player
+{
+};
+
+#endif // THE_SHINING_GAME_PROJECT_237_PLAYER_H
